6166cbb646b068f4c978a86e00b8317cb914055e
alwinsolanky <alwinsolanky@gmail.com>
2016-08-05 10:39:05 +0100

    JQuery Introduction

--- interface.js
+++ interface.js
@@ -34,6 +34,20 @@ $(document).ready(function(){
     updateTemperature();
   });
 
+  $('form').on("submit", function(event){
+    event.preventDefault();
+    displayWeather($(this).serialize());
+  });
 
+  function displayWeather(city)
+  {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?';
+    var token = '&appid=733539c7a2e636a87f14365fc38fdaa8';
+    var units = '&units=metric';
+    $.get(url + city + token + units, function(data)
+    {
+      $('#city-temperature').text(data.main.temp);
+    });
+  }
 
 });
